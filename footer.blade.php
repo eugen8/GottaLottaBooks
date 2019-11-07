5b23d3bdeaf8eefe5af2c030c29b4e1179c44529
stbanks <steven.banks@grandview.edu>
2019-11-07 13:27:13 -0600

    messing with footer page

---
+++ footer.blade.php
@@ -0,0 +1,45 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Footer</title>
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+        <style>
+            .foot a{
+                text-decoration: none;
+                color: black;
+            }
+
+            .foot a:visited{
+                text-decoration: none;
+            }
+
+            .foot a:hover{
+                text-decoration: underline;
+                color: blue;
+            }
+        </style>
+
+    </head>
+    <body>
+        <div class="foot">
+            <p>
+                &copy; 2019 GottaLottaBooks.com
+            </p>
+            <ul>
+                <li>About Us</li>
+                <li>Contact</li>
+                <li><a href="https://github.com/eugen8/GottaLottaBooks/tree/landing-page" target="_Blank">Git Repository</a></li>
+            </ul>
+        </div>
+    </body>
+</html>
