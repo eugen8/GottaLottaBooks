c415604c30ace08bc050179a01e99f9d8661c52b
mai huynh <maihuynh.com>
2019-11-11 22:04:41 -0600

    landing page with books

--- public/js/app.js
+++ public/js/app.js
@@ -49350,6 +49350,8 @@ module.exports = function(module) {
  */
 __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 
+__webpack_require__(/*! ./test */ "./resources/js/test.js");
+
 window.Vue = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.common.js");
 /**
  * The following block of code may be used to automatically register your
@@ -49488,6 +49490,17 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/js/test.js":
+/*!******************************!*\
+  !*** ./resources/js/test.js ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+window.alert('hello there');
+
+/***/ }),
+
 /***/ "./resources/sass/app.scss":
 /*!*********************************!*\
   !*** ./resources/sass/app.scss ***!
@@ -49506,8 +49519,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Eugen-hp\Desktop\grandview\GottaLottaBooks\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Eugen-hp\Desktop\grandview\GottaLottaBooks\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\nhuma\Desktop\GRANDVIEW\web_application\dev\GottaLottaBooks\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\nhuma\Desktop\GRANDVIEW\web_application\dev\GottaLottaBooks\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
