1a249ee1114f903f9e0a92573b6dd0309e4388fc
maihuynh91 <42097295+maihuynh91@users.noreply.github.com>
2019-11-19 15:29:13 -0600

    Maihuynh (#13)
    * no auth
    
    * seed new books
    
    * seed books
    
    * fixed branch routes

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,9 @@
 
 
 Auth::routes();
-Route::get('/landingPage', 'LandingController@index')->name('landingPage');
-Route::get('/', 'HomeController@index')->name('home');
-Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', 'LandingController@index')->name('landingPage');
+Route::get('/home', 'LandingController@index')->name('landingPage');
 Route::get('/logout', 'HomeController@logout');
 
 
