a7b378a9ea37fb33ede4b07577a557096c78052d
stbanks <steven.banks@grandview.edu>
2019-11-19 15:01:34 -0600

    created _footer.scss, updated app.scss, and updated footer.blade.php

--- resources/views/layout/partials/footer.blade.php
+++ resources/views/layout/partials/footer.blade.php
@@ -1,7 +1,55 @@
-<div>
-    <ul>
-        <li>Terms of Service</li>
-        <li>Privacy Policy</li>
-        <li>About</li>
-    </ul>
-</div>
+<!-- Footer -->
+        <section id="footer">
+            <div class="container">
+                <div class="row text-center text-xs-center text-sm-left text-md-left">
+                    <div class="col-xs-12 col-sm-4 col-md-4">
+                        <h5>Quick Links</h5>
+                        <ul class="list-unstyled quick-links">
+                            <li>>Home</li>
+                            <li>>About</li>
+                            <li>>FAQ</li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-12 col-sm-4 col-md-4">
+                        <h5>Quick Links</h5>
+                        <ul class="list-unstyled quick-links">
+                            <li>>Sign Up</li>
+                            <li>>Partners</li>
+                            <li>>Careers</li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-12 col-sm-4 col-md-4">
+                        <h5>Quick Links</h5>
+                        <ul class="list-unstyled quick-links">
+                            <li>>Blog</li>
+                            <li>>Des Moines Libraries</li>
+                            <li><a href="https://github.com/eugen8/GottaLottaBooks" target="_blank">>Git Repository</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+                        <ul class="list-unstyled list-inline social text-center">
+                            <li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-facebook"></i></a></li>
+                            <li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-twitter"></i></a></li>
+                            <li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-instagram"></i></a></li>
+                            <li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-google-plus"></i></a></li>
+                            <li class="list-inline-item"><a href="javascript:void();" target="_blank"><i class="fa fa-envelope"></i></a></li>
+                        </ul>
+                    </div>
+                    </hr>
+                </div>	
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+                        <p><u>Gotta Lotta Books</u> is a trademark of Trading Pages, Inc.</p>
+                        <p class="h6">&copy 2019 Gotta Lotta Books.</p>
+                    </div>
+                    </hr>
+                </div>	
+            </div>
+        </section>
+	<!-- ./Footer -->                      
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
