1aa19b5bee2d7aa8898b86ddbed55124f1575c9c
stbanks <54864929+stbanks@users.noreply.github.com>
2019-11-19 15:15:08 -0600

    Footerv1 (#12)
    * messing with footer page
    
    * trying to get my localhost working
    
    * updating footer
    
    * created _footer.scss, updated app.scss, and updated footer.blade.php
    
    * Hopefully this will be everything

--- resources/views/layout/partials/footer.blade.php
+++ resources/views/layout/partials/footer.blade.php
@@ -1,7 +1,51 @@
-<div>
-    <ul>
-        <li>Terms of Service</li>
-        <li>Privacy Policy</li>
-        <li>About</li>
-    </ul>
-</div>
+<!-- Footer -->
+        <section id="footer">
+            <div class="container">
+                <div class="row text-center text-xs-center text-sm-left text-md-left">
+                    <div class="col-xs-12 col-sm-4 col-md-4">
+                        <h5>Quick Links</h5>
+                        <ul class="list-unstyled quick-links">
+                            <li>>Home</li>
+                            <li>>About</li>
+                            <li>>FAQ</li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-12 col-sm-4 col-md-4">
+                        <h5>Quick Links</h5>
+                        <ul class="list-unstyled quick-links">
+                            <li>>Sign Up</li>
+                            <li>>Partners</li>
+                            <li>>Careers</li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-12 col-sm-4 col-md-4">
+                        <h5>Quick Links</h5>
+                        <ul class="list-unstyled quick-links">
+                            <li>>Blog</li>
+                            <li>>Des Moines Libraries</li>
+                            <li><a href="https://github.com/eugen8/GottaLottaBooks" target="_blank">>Git Repository</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+                        <ul class="list-unstyled list-inline social text-center">
+                            <li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-facebook"></i></a></li>
+                            <li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-twitter"></i></a></li>
+                            <li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-instagram"></i></a></li>
+                            <li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-google-plus"></i></a></li>
+                            <li class="list-inline-item"><a href="javascript:void();" target="_blank"><i class="fa fa-envelope"></i></a></li>
+                        </ul>
+                    </div>
+                    </hr>
+                </div>	
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+                        <p><u>Gotta Lotta Books</u> is a trademark of Trading Pages, Inc.</p>
+                        <p class="h6">&copy 2019 Gotta Lotta Books.</p>
+                    </div>
+                    </hr>
+                </div>	
+            </div>
+        </section>
+	<!-- ./Footer -->                      
