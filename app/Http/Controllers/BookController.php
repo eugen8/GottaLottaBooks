85ae095037b9411f6df651e75f2727baff5b9da0
Hienhtho <37109335+Hienhtho@users.noreply.github.com>
2019-11-25 17:56:54 -0600

    bookDetails

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+use DB;
+use View;
+
+class BookController extends Controller
+{
+    public function index($id) {
+        $book = DB::table('book')->where('id', $id)->first();
+
+        return view('bookDetails')->with(['book'=>$book]);
+    }
+}
+
+?>
