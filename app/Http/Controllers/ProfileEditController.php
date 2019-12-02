082b42360660adbc2c842fde25de66dbf389fc42
Hienhtho <37109335+Hienhtho@users.noreply.github.com>
2019-12-02 09:20:31 -0600

    profileEdit_backend

--- app/Http/Controllers/ProfileEditController.php
+++ app/Http/Controllers/ProfileEditController.php
@@ -20,20 +20,13 @@ class ProfileEditController extends Controller
 
     public function update(Request $request)
     {
-        // $request is our form data that was submitted
-        // Get the current user
         $user = Auth::user();
-        // set the name of the user to the name from the form data
         $user->name = $request->input('name');
-        // set the email of the user to the email from the form data
         $user->email = $request->input('email');
-        // set the zip of the user to the zip from the form data
         $user->zip =$request->input('zip');
-       
-        // save the user to the database
+
         $user->save();
 
-        // return to the page
         return back();
     }
 }
