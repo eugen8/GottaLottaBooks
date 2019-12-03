94df2955d2f3bb4dd0470cd194c9bb9a090601c5
lilazn71 <michael.nguyen@grandview.edu>
2019-12-03 16:07:54 -0600

    hola4

--- resources/views/layout/partials/nav.blade.php
+++ resources/views/layout/partials/nav.blade.php
@@ -11,12 +11,6 @@
           <li class="nav-item active">
               <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
           </li>
-          <li class="nav-item">
-              <a class="nav-link" href="/glb/profile">Edit Your Profile</a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="/glb/userprofile">View Your Profile</a>
-          </li>
         </ul>
         <form class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
