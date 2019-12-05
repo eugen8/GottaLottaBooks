8486e3fb6def526266438e215dc7b14ff548a6f5
stbanks <54864929+stbanks@users.noreply.github.com>
2019-12-05 15:21:11 -0600

    Steven (#26)
    * changed the home button ahref
    
    * changed some links in the footer as well

--- resources/views/layout/partials/nav.blade.php
+++ resources/views/layout/partials/nav.blade.php
@@ -1,7 +1,7 @@
 
 
 <nav class="custom-navbar navbar navbar-expand-lg navbar-light">
-    <a class="navbar-brand" id="glb" href="#">GottaLottaBooks</a>
+    <a class="navbar-brand" id="glb" href="{{url('/')}}">GottaLottaBooks</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -9,7 +9,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto" id="navLinks">
           <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="{{url('/')}}">Home <span class="sr-only">(current)</span></a>
           </li>
         </ul>
         <form class="form-inline my-2 my-lg-0">
